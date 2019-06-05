24b7ab3c9af2b7c955cdb66e73ed07dcebe2ad90
Vinal Patel <vinalpatel18@gmail.com>
2019-06-05 14:22:59 -0400

    inital commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Seachbar from './searchbar'
+
+class App extends React.Component{
+    render() {
+        return (<div>Hello 
+            <Seachbar/>
+        </div>)
+    }
+}
+
+export default App
